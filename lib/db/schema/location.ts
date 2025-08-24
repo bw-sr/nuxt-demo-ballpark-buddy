b7d82e6718c254f1cbb207a95a7870f0733f0491
Ben Wozniak <bw@sports-reference.com>
2025-08-24 08:05:02 -0500

    setup auth

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,5 +1,7 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const locations = sqliteTable("locations", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
@@ -7,6 +9,7 @@ export const locations = sqliteTable("locations", {
   description: text(),
   lat: real().notNull(),
   long: real().notNull(),
+  userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$onUpdate(() => Date.now()),
 });
