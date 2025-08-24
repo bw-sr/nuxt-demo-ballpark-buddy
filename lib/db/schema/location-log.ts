1e411a1eaa67e460521111faeb3eaeb63f43b4e3
bw-sr <bw@sports-reference.com>
2025-08-24 08:14:23 -0500

    setup auth (#52)
    * setup auth
    
    * dont lint on build

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -1,5 +1,6 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { locations } from "./location";
 
 export const locationLogs = sqliteTable("locationLogs", {
@@ -10,6 +11,7 @@ export const locationLogs = sqliteTable("locationLogs", {
   endedAt: int().notNull(),
   lat: real().notNull(),
   long: real().notNull(),
+  userId: int().notNull().references(() => user.id),
   locationId: int().notNull().references(() => locations.id, { onDelete: "cascade" }),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
